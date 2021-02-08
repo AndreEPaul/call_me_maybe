66eba19e763f50ba9b292f4f1460b1bbadc23676
Andre Paul <andrepaul8128@gmail.com>
2021-02-08 15:02:19 -0800

    changed color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.purple[400],
+        accentColor: Colors.grey[500],
+      ),
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
