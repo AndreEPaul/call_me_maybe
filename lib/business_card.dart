b268b2bda960ded78ae8c921d26c1033522fcb37
Andre Paul <andrepaul8128@gmail.com>
2021-02-09 13:23:59 -0800

    photo added, may have centering issues

--- lib/business_card.dart
+++ lib/business_card.dart
@@ -15,37 +15,45 @@ _launchURL() async {
   }
 }
 
+const profilePic = Image(
+  image: NetworkImage('https://1.bp.blogspot.com/-WT2mY0rIVDo/WnJyJ554ojI/AAAAAAAAC4c/uljObUd7dAwoxffuVTgc4DWfTos7nLi_ACLcBGAs/s320/Snapshot%2B-%2B15.png')
+);
+
 class BusinessCard extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Placeholder(
-          fallbackHeight: 100.0,
-          fallbackWidth: 10.0,
-        ),
-        Padding(
+      body: Column(
+        // TODO: potential centering issue, but this is possibly just on web
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
             padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: Text('Andre Paul',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ))),
-        Padding(
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: Text('Software Engineer')),
-        Padding(
-          //TODO: change this to redirect to SMS message
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: Text('555-555-5555')),
-        ElevatedButton(
-          onPressed: _launchURL, 
-          child: Text('GitHub Profile')
-        ),
-        Padding(
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: Text('example@email.com')),
-      ],
-    ));
+            child: profilePic,
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text('Andre Paul',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ))),
+          Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text('Software Engineer')),
+          Padding(
+            //TODO: change this to redirect to SMS message
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text('555-555-5555')),
+          ElevatedButton(
+            onPressed: _launchURL, 
+            child: Text('GitHub Profile')
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text('example@email.com')),
+        ],
+      ),
+    );
   }
 }
