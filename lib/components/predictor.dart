9f533b818c99a82a536d37810f0ea4bf99c46475
Andre Paul <andrepaul8128@gmail.com>
2021-02-11 15:01:02 -0800

    predictor working

--- lib/components/predictor.dart
+++ lib/components/predictor.dart
@@ -2,11 +2,52 @@ import 'package:flutter/material.dart';
 
 import '../models/predictor_data.dart';
 
-class Predictor extends StatelessWidget {
-  final predictorData = PredictorData();
+class Predictor extends StatefulWidget {
+  @override
+  _PredictState createState() => _PredictState();
+}
 
+class _PredictState extends State<Predictor> {
+  static final predictorData = PredictorData();
+  final _padding = const EdgeInsets.all(10);
+  final _mainAlign = MainAxisAlignment.center;
+  final _crossAlign = CrossAxisAlignment.center;
+  String currentPrediction = predictorData.pickPrediction();
   @override
   Widget build(BuildContext context) {
-    return Text(predictorData.pickPrediction());
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: _mainAlign,
+          crossAxisAlignment: _crossAlign,
+          children: [
+            Padding(
+                padding: _padding,
+                child: const Text(
+                  'Call Me... Maybe?',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                )),
+            Padding(
+                padding: _padding,
+                child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        currentPrediction = predictorData.pickPrediction();
+                      });
+                    },
+                    child: const Text('Ask a question... tap for the answer.',
+                        style: TextStyle(
+                            fontSize: 18, fontStyle: FontStyle.italic)))),
+            Padding(
+                padding: _padding,
+                child: Text(
+                  currentPrediction,
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
