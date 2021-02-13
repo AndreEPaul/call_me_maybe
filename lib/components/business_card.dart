bd7dc2261d4941a8e024fec50ad2cabb0346c3fe
Andre Paul <andrepaul8128@gmail.com>
2021-02-12 17:33:46 -0800

    complete

--- lib/components/business_card.dart
+++ lib/components/business_card.dart
@@ -19,52 +19,120 @@ Future _launchTextMsg() async {
   }
 }
 
-const profilePic = Image(
-    image: NetworkImage(
-        'https://1.bp.blogspot.com/-WT2mY0rIVDo/WnJyJ554ojI/AAAAAAAAC4c/uljObUd7dAwoxffuVTgc4DWfTos7nLi_ACLcBGAs/s320/Snapshot%2B-%2B15.png'));
-
 class BusinessCard extends StatelessWidget {
-  final _padding = const EdgeInsets.all(10);
   final _mainAlign = MainAxisAlignment.center;
   final _crossAlign = CrossAxisAlignment.center;
+  final _padding = const EdgeInsets.all(8);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-          child: Column(
-        mainAxisAlignment: _mainAlign,
-        crossAxisAlignment: _crossAlign,
-        children: [
-          Padding(
-            padding: _padding,
-            child: profilePic,
-          ),
-          Padding(
-              padding: _padding,
-              child: const Text('Andre Paul',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ))),
-          Padding(padding: _padding, child: const Text('Software Engineer')),
-          Padding(
-              padding: _padding,
-              child: const ElevatedButton(
-                  onPressed: _launchTextMsg, child: Text('(555)-123-4567'))),
-          Padding(
-            padding: _padding,
-            child: Row(
-              mainAxisAlignment: _mainAlign,
-              children: [
-                const ElevatedButton(
-                    onPressed: _launchGithub, child: Text('GitHub Profile')),
-                Padding(
-                    padding: _padding, child: const Text('example@email.com')),
-              ],
-            ),
-          ),
-        ],
-      )),
-    );
+    if (!isLandscape(context)) {
+      return FractionallySizedBox(
+        widthFactor: 0.9,
+        heightFactor: 0.9,
+        child: Padding(
+            padding: EdgeInsets.all(padding(context)),
+            child: Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: _mainAlign,
+                    crossAxisAlignment: _crossAlign,
+                    children: [
+                      SizedBox(
+                        width: 250,
+                        height: 250,
+                        child: Image.asset('assets/callmemaybe.png'),
+                      ),
+                      Padding(
+                          padding: _padding,
+                          child: const Text('Andre Paul',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ))),
+                      Padding(
+                          padding: _padding,
+                          child: const Text('Software Engineer')),
+                      Padding(
+                          padding: _padding,
+                          child: const ElevatedButton(
+                              onPressed: _launchTextMsg,
+                              child: Text('(555)-123-4567'))),
+                      Padding(
+                          padding: _padding,
+                          child: const ElevatedButton(
+                              onPressed: _launchGithub,
+                              child: Text('GitHub Profile'))),
+                      Padding(
+                          padding: _padding,
+                          child: const Text('example@email.com')),
+                    ]),
+              ),
+            )),
+      );
+    } else {
+      return FractionallySizedBox(
+        widthFactor: 0.9,
+        heightFactor: 0.9,
+        child: Padding(
+            padding: EdgeInsets.all(padding(context)),
+            child: Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: SingleChildScrollView(
+                child: Row(
+                    mainAxisAlignment: _mainAlign,
+                    crossAxisAlignment: _crossAlign,
+                    children: [
+                      SizedBox(
+                        width: 150,
+                        height: 150,
+                        child: Image.asset('assets/callmemaybe.png'),
+                      ),
+                      Column(children: [
+                        Padding(
+                            padding: _padding,
+                            child: const Text('Andre Paul',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ))),
+                        Padding(
+                            padding: _padding,
+                            child: const Text('Software Engineer'))
+                      ]),
+                      Column(children: [
+                        Padding(
+                            padding: _padding,
+                            child: const ElevatedButton(
+                                onPressed: _launchTextMsg,
+                                child: Text('(555)-123-4567'))),
+                        Padding(
+                            padding: _padding,
+                            child: const ElevatedButton(
+                                onPressed: _launchGithub,
+                                child: Text('GitHub Profile'))),
+                        Padding(
+                            padding: _padding,
+                            child: const Text('example@email.com'))
+                      ]),
+                    ]),
+              ),
+            )),
+      );
+    }
+  }
+}
+
+double padding(BuildContext context) {
+  if (MediaQuery.of(context).orientation == Orientation.landscape) {
+    return MediaQuery.of(context).size.width * 0.05;
+  } else {
+    return MediaQuery.of(context).size.width * 0.1;
+  }
+}
+
+bool isLandscape(BuildContext context) {
+  if (MediaQuery.of(context).orientation == Orientation.landscape) {
+    return true;
+  } else {
+    return false;
   }
 }
--- lib/components/predictor.dart
+++ lib/components/predictor.dart
@@ -9,45 +9,116 @@ class Predictor extends StatefulWidget {
 
 class _PredictState extends State<Predictor> {
   static final predictorData = PredictorData();
-  final _padding = const EdgeInsets.all(10);
   final _mainAlign = MainAxisAlignment.center;
   final _crossAlign = CrossAxisAlignment.center;
   String currentPrediction = predictorData.pickPrediction();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: _mainAlign,
-          crossAxisAlignment: _crossAlign,
-          children: [
-            Padding(
-                padding: _padding,
-                child: const Text(
-                  'Call Me... Maybe?',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                )),
-            Padding(
-                padding: _padding,
-                child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        currentPrediction = predictorData.pickPrediction();
-                      });
-                    },
-                    child: const Text('Ask a question... tap for the answer.',
+    if (!isLandscape(context)) {
+      return FractionallySizedBox(
+          widthFactor: 0.9,
+          heightFactor: 0.9,
+          child: Scaffold(
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: _mainAlign,
+                crossAxisAlignment: _crossAlign,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(padding(context)),
+                      child: const Text(
+                        'Call Me... Maybe?',
                         style: TextStyle(
-                            fontSize: 18, fontStyle: FontStyle.italic)))),
-            Padding(
-                padding: _padding,
-                child: Text(
-                  currentPrediction,
-                  style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
-                )),
-          ],
-        ),
-      ),
-    );
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.all(padding(context)),
+                      child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              currentPrediction =
+                                  predictorData.pickPrediction();
+                            });
+                          },
+                          child: const Text(
+                              'Ask a question... tap for the answer.',
+                              style: TextStyle(
+                                  fontSize: 18, fontStyle: FontStyle.italic)))),
+                  Padding(
+                      padding: EdgeInsets.all(padding(context)),
+                      child: Text(
+                        currentPrediction,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      )),
+                ],
+              ),
+            ),
+          ));
+    } else {
+      return FractionallySizedBox(
+          widthFactor: 0.9,
+          heightFactor: 0.9,
+          child: Scaffold(
+            body: SingleChildScrollView(
+              child: Row(
+                mainAxisAlignment: _mainAlign,
+                crossAxisAlignment: _crossAlign,
+                children: [
+                  Column(children: [
+                    Padding(
+                        padding: EdgeInsets.all(padding(context)),
+                        child: const Text(
+                          'Call Me... Maybe?',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        )),
+                    Padding(
+                        padding: EdgeInsets.all(padding(context)),
+                        child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                currentPrediction =
+                                    predictorData.pickPrediction();
+                              });
+                            },
+                            child: const Text(
+                                'Ask a question... tap for the answer.',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontStyle: FontStyle.italic))))
+                  ]),
+                  Padding(
+                      padding: EdgeInsets.all(padding(context)),
+                      child: SizedBox(
+                        height: 250,
+                        width: 200,
+                        child: Text(
+                        currentPrediction,
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ))),
+                ],
+              ),
+            ),
+          ));
+    }
+    ;
+  }
+}
+
+double padding(BuildContext context) {
+  if (MediaQuery.of(context).orientation == Orientation.landscape) {
+    return MediaQuery.of(context).size.width * 0.05;
+  } else {
+    return MediaQuery.of(context).size.width * 0.1;
+  }
+}
+
+bool isLandscape(BuildContext context) {
+  if (MediaQuery.of(context).orientation == Orientation.landscape) {
+    return true;
+  } else {
+    return false;
   }
 }
